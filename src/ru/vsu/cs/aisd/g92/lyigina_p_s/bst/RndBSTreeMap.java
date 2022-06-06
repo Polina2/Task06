0cead282b4f07258c2f033f76066cc1a62a4cd4a
Haku <polina231003@mail.ru>
2022-06-06 12:33:46 +0300

    RndBSTreeMap done

--- src/ru/vsu/cs/aisd/g92/lyigina_p_s/bst/RndBSTreeMap.java
+++ src/ru/vsu/cs/aisd/g92/lyigina_p_s/bst/RndBSTreeMap.java
@@ -1,4 +1,10 @@
 package ru.vsu.cs.aisd.g92.lyigina_p_s.bst;
 
-public class RndBSTreeMap {
+public class RndBSTreeMap<K extends Comparable<K>, V> implements BSTreeMap<K, V> {
+    private final BSTree<MapTreeEntry<K, V>> tree = new RndBSTree<>();
+
+    @Override
+    public BSTree<MapTreeEntry<K, V>> getTree() {
+        return tree;
+    }
 }
